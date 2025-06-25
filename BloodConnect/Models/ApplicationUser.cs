5f45c43343ae7044ae59735933b7c7bbe0a2afbe
dabananda <imdmitra@gmail.com>
2025-06-25 12:26:57 +0600

    Customizing Identity User

---
+++ BloodConnect/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BloodConnect.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+        public string StudentId { get; set; }
+        public string Department { get; set; }
+        public string Session { get; set; }
+        public string BloodGroup { get; set; }
+        public DateTime? LastDonationDate { get; set; }
+        public bool IsAvailable { get; set; }
+        public int TotalPoints { get; set; }
+        public bool IsApprovedByAdmin { get; set; }
+        public DateTime RegisteredAt { get; set; }
+    }
+}
