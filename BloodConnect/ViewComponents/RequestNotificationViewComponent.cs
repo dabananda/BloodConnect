30e84d98c6c975b365d3998e19b0c7fc37fc202c
dabananda <imdmitra@gmail.com>
2025-07-05 19:02:33 +0600

    Notification badge

---
+++ BloodConnect/ViewComponents/RequestNotificationViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using BloodConnect.Data;
+using BloodConnect.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BloodConnect.ViewComponents
+{
+    public class RequestNotificationViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public RequestNotificationViewComponent(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (!User.Identity.IsAuthenticated) return View(0);
+
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            var pendingCount = await _context.BloodRequests
+                .CountAsync(r => r.DonorId == user.Id && r.Status == RequestStatus.Pending);
+
+            return View(pendingCount);
+        }
+    }
+}
---
+++ BloodConnect/ViewComponents/SentRequestNotificationViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using BloodConnect.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BloodConnect.Models;
+using System.Threading.Tasks;
+
+namespace BloodConnect.ViewComponents
+{
+    public class SentRequestNotificationViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public SentRequestNotificationViewComponent(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            if (user == null) return Content("");
+
+            var count = await _context.BloodRequests
+                .Where(r => r.RequestorId == user.Id && r.Status == RequestStatus.Accepted)
+                .CountAsync();
+
+            return View(count);
+        }
+    }
+}
